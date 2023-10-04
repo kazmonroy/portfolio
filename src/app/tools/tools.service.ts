d4d36fe24eed5872bd9ed5b1c254148a0bd7b144
kazmonroy <katherinemonroy@gmail.com>
2023-10-05 00:15:38 +0200

    Update tools icons

--- src/app/tools/tools.service.ts
+++ src/app/tools/tools.service.ts
@@ -15,9 +15,9 @@ export class ToolsService {
     { name: 'TailwindCSS', toolsvg: 'tailwindcss.svg' },
     { name: 'CSS', toolsvg: 'css3.svg' },
 
-    { name: 'MongoDB', toolsvg: 'mongodb.svg' },
-    { name: 'Github', toolsvg: 'github.svg' },
-    { name: 'Next.js', toolsvg: 'nextdotjs.svg' },
-    { name: 'Vercel', toolsvg: 'vercel.svg' },
+    // { name: 'MongoDB', toolsvg: 'mongodb.svg' },
+    // { name: 'Github', toolsvg: 'github.svg' },
+    // { name: 'Next.js', toolsvg: 'nextdotjs.svg' },
+    // { name: 'Vercel', toolsvg: 'vercel.svg' },
   ];
 }
