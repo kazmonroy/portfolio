213787884adf2662bfa2ab75b5bf0a744c584fad
kazmonroy <katherinemonroy@gmail.com>
2023-10-04 13:48:54 +0200

    Reuse showcase component in Projects Page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../shared/components/card/card.component';
 import { DataService } from '../shared/services/data.service';
+import { ShowcaseComponent } from '../showcase/showcase.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, ShowcaseComponent],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
