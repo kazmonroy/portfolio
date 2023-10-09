3e4b6eed2a352ce67131a1403d457fad036726f3
kazmonroy <katherinemonroy@gmail.com>
2023-10-09 22:19:30 +0200

    Add role-card component in Opal Project

---
+++ src/app/shared/components/color-card/color-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface ColorRole {
+  class: string;
+  bg: string;
+  title: string;
+  roles: string[];
+  hexcode: string;
+}
+@Component({
+  selector: 'app-color-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './color-card.component.html',
+  styleUrls: ['./color-card.component.scss'],
+})
+export class ColorCardComponent {
+  @Input() colorRole!: ColorRole;
+}
