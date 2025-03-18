b798c4e45990b4c36aa3c1777ca586ab5c203e4a
Katherine Monroy <katherinemonroy@gmail.com>
2025-03-19 00:52:35 +0100

    Update toolbox icons

--- src/app/tools/tools.service.ts
+++ src/app/tools/tools.service.ts
@@ -5,19 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class ToolsService {
   tools = [
-    { name: 'Node.js', toolsvg: 'nodedotjs.svg' },
-    { name: 'Angular', toolsvg: 'angular.svg' },
+    { name: '.NET', toolsvg: 'dotnet.svg' },
     { name: 'React', toolsvg: 'react.svg' },
     { name: 'TypeScript', toolsvg: 'typescript.svg' },
+    { name: 'Vue', toolsvg: 'vue.svg' },
 
-    { name: 'Sass', toolsvg: 'sass.svg' },
     { name: 'Figma', toolsvg: 'figma.svg' },
-    { name: 'Tailwind', toolsvg: 'tailwindcss.svg' },
+    { name: 'Angular', toolsvg: 'angular.svg' },
+    { name: 'Docker', toolsvg: 'docker.svg' },
     { name: 'CSS', toolsvg: 'css3.svg' },
-
-    // { name: 'MongoDB', toolsvg: 'mongodb.svg' },
-    // { name: 'Github', toolsvg: 'github.svg' },
-    // { name: 'Next.js', toolsvg: 'nextdotjs.svg' },
-    // { name: 'Vercel', toolsvg: 'vercel.svg' },
   ];
 }
